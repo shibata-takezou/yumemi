1a31704d85eafa4c8180bbb9f7d6e10443741959
shibata-takezou <takezou0317@gmail.com>
2023-07-10 20:37:10 +0900

    APIのバージョンとユーザの入力フォームの追加

--- yumemi/ContentView.swift
+++ yumemi/ContentView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+
+//送信JSON
 struct User: Codable{
     var name:String
     var birthday:YearMonthDay
@@ -17,89 +19,69 @@ struct YearMonthDay:Codable{
     var month: Int
     var day: Int
 }
-struct Response:Codable{
-    var name:String
-    var capital:String
-    var citizen_day: MonthDay?
-    var has_coast_line:Bool
-    var logo_url:String
-    var brief:String
-}
-struct MonthDay:Codable{
-    var month: Int
-    var day: Int
-}
-
-func postUser(user: User, completion: @escaping (Response?) -> Void) {//非同期で文字を表示
-    guard let url = URL(string: "https://yumemi-ios-junior-engineer-codecheck.app.swift.cloud/my_fortune?API-Version=v1") else {
-        completion(nil)
-        return
-    }
-
-    var request = URLRequest(url: url)
-    request.httpMethod = "POST"
-    request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
-
-    do {
-        //JSONのエンコード
-        let jsonEncoder = JSONEncoder()
-        let jsonData = try jsonEncoder.encode(user)
-        request.httpBody = jsonData
-
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let error = error {
-                print("Error: \(error.localizedDescription)")
-                completion(nil)
-                return
-            }
-
-            if let data = data {
-                do {
-                    //JSONのデコード
-                    let jsonDecoder = JSONDecoder()
-                    let responseData = try jsonDecoder.decode(Response.self, from: data)
-                    completion(responseData)
-                } catch {
-                    print("Error decoding response data: \(error)")
-                    completion(nil)
-                }
-            }
-        }.resume()
-    } catch {
-        print("Error encoding user: \(error)")
-        completion(nil)
-    }
-}
 
 struct ContentView: View {
-    @State private var responseData: Response?
+    @State private var user: User = User(name: "", birthday: YearMonthDay(year: 0, month: 0, day: 0), blood_type: "", today: YearMonthDay(year: 0, month: 0, day: 0))
+    @State private var selectionDate = Date()
+    @State private var selectedYear = 0
+    @State private var selectedMonth = 0
+    @State private var selectedDay = 0
+    @State private var todayYear = 0
+    @State private var todayMonth = 0
+    @State private var todayDay = 0
+    @State private var userName = ""
+    @State private var blood = "a"
 
     var body: some View {
-        VStack {
-            if let responseData = responseData {
-                Text("Name: \(responseData.name)")
-                Text("Brief: \(responseData.brief)")
-                Text("Capital: \(responseData.capital)")
-                if let citizenDay = responseData.citizen_day {
-                    Text("Citizen Day: \(citizenDay.month)/\(citizenDay.day)")
+            NavigationStack{
+                VStack (alignment: .center){
+                    HStack{
+                        Text("氏名")
+                        Spacer()
+                        TextField("氏名を入力してください", text: $userName)
+                            .multilineTextAlignment(TextAlignment.trailing)
+                    }
+                    DatePicker("生年月日", selection: $selectionDate, displayedComponents: .date).environment(\.locale, Locale(identifier: "ja_JP"))
+                    
+                    HStack{
+                        Text("血液型")
+                        Spacer()
+                        Picker(selection: $blood, label: Text("-")) {
+                            Text("A").tag("a")
+                            Text("B").tag("b")
+                            Text("O").tag("o")
+                            Text("AB").tag("ab")
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                NavigationLink(destination: ResView(user: $user)) {
+                    Text("送信")
                 }
-                Text("Has Coast Line: \(String(responseData.has_coast_line))")
-                Text("Logo URL: \(responseData.logo_url)")
-            } else {
-                Text("Loading...")
+                .padding()
+                .simultaneousGesture(TapGesture().onEnded {
+                    //今日の年月日
+                    let calendar = Calendar.current
+                    var dateComponents = calendar.dateComponents([.year, .month, .day], from: Date())
+                    todayYear = dateComponents.year ?? 0
+                    todayMonth = dateComponents.month ?? 0
+                    todayDay = dateComponents.day ?? 0
+                    
+                    //生年月日
+                    dateComponents = calendar.dateComponents([.year, .month, .day], from: selectionDate)
+                    selectedYear = dateComponents.year ?? 0
+                    selectedMonth = dateComponents.month ?? 0
+                    selectedDay = dateComponents.day ?? 0
+                    
+                    user = User(
+                        name: userName,
+                        birthday: YearMonthDay(year: selectedYear, month: selectedMonth, day: selectedDay),
+                        blood_type: blood,
+                        today: YearMonthDay(year: todayYear, month: todayMonth, day: todayDay)
+                    )
+                })
             }
-        }
-        .onAppear {
-            let user = User(
-                name: "柴田武蔵",
-                birthday: YearMonthDay(year: 2003, month: 3, day: 17),
-                blood_type: "a",
-                today: YearMonthDay(year: 2023, month: 7, day: 10)
-            )
-            print(user)
-            postUser(user: user) { response in
-                self.responseData = response
-            }
+
+            
         }
         .padding()
     }
---
+++ yumemi/ResView.swift
@@ -0,0 +1,97 @@
+//
+//  ResView.swift
+//  yumemi
+//
+//  Created by 柴田武蔵 on 2023/07/10.
+//
+
+import SwiftUI
+//受信JSON
+struct Response:Codable{
+    var name:String
+    var capital:String
+    var citizen_day: MonthDay?
+    var has_coast_line:Bool
+    var logo_url:String
+    var brief:String
+}
+struct MonthDay:Codable{
+    var month: Int
+    var day: Int
+}
+
+func postUser(user: User, completion: @escaping (Response?) -> Void) {//非同期で文字を表示
+    let version = "v1"
+    let urlstr = "https://yumemi-ios-junior-engineer-codecheck.app.swift.cloud/my_fortune?API-Version=\(version)"
+    guard let url = URL(string: urlstr) else {
+        completion(nil)
+        return
+    }
+
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+
+    do {
+        //JSONのエンコード
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try jsonEncoder.encode(user)
+        request.httpBody = jsonData
+
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                completion(nil)
+                return
+            }
+
+            if let data = data {
+                do {
+                    //JSONのデコード
+                    let jsonDecoder = JSONDecoder()
+                    let responseData = try jsonDecoder.decode(Response.self, from: data)
+                    completion(responseData)
+                } catch {
+                    print("Error decoding response data: \(error)")
+                    completion(nil)
+                }
+            }
+        }.resume()
+    } catch {
+        print("Error encoding user: \(error)")
+        completion(nil)
+    }
+}
+struct ResView: View {
+    @Binding var user: User
+    @State private var responseData: Response?
+    var body: some View{
+        
+        VStack {
+            if let responseData = responseData {
+                Text("Name: \(responseData.name)")
+                Text("Brief: \(responseData.brief)")
+                Text("Capital: \(responseData.capital)")
+                if let citizenDay = responseData.citizen_day {
+                    Text("Citizen Day: \(citizenDay.month)/\(citizenDay.day)")
+                }
+                Text("Has Coast Line: \(String(responseData.has_coast_line))")
+                Text("Logo URL: \(responseData.logo_url)")
+            } else {
+                Text("Loading...")
+            }
+        }
+        .onAppear {
+            print(user)
+            postUser(user: user) { response in
+                self.responseData = response
+            }
+        }
+    }
+}
+
+struct ResView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
